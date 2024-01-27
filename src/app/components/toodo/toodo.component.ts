6ea170950a2fef031f67ef8c93efa9b35ecc8a77
marcosestebandev <marcosestebandev@gmail.com>
2024-01-27 16:50:23 -0300

    Add style-css toodolist

--- src/app/components/toodo/toodo.component.ts
+++ src/app/components/toodo/toodo.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ToodoModel } from '../../models/toodo';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toodo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toodo.component.html',
   styleUrl: './toodo.component.css'
 })
 export class ToodoComponent {
+  toodolist = signal<ToodoModel[]>([
+    {
+      id:1,
+      title:'Buy',
+      completed: false,
+      editing:false,
+    },
+    {
+      id:2,
+      title:'sell',
+      completed:true,
+      editing:false,
+    },
+    {
+      id:3,
+      title:'send',
+      completed:false,
+      editing:false,
+    }
+  ]);
+  filter = signal<FilterType>('all');
 
 }
